ad9c504dde3abfa41c9d3a1d8f1e101835e9ae94
Michael Durkan <durkanmick@gmail.com>
2025-09-07 05:43:04 +0000

    [Sync Iteration] java/annalyns-infiltration/1

---
+++ solutions/java/annalyns-infiltration/1/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,21 @@
+class AnnalynsInfiltration {
+    // Return true if Knight is sleeping, false otherwise
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    // Return true if anyone is awake, false otherwise
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+   
+    // Return true if Prisoner awake and Archer sleeping, else false
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return prisonerIsAwake && !archerIsAwake;
+    }
+
+    // Return true if dogPresent and archer asleep, or prisoner awake and knight/archer sleeping, else false
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        return (petDogIsPresent && !archerIsAwake ) || (prisonerIsAwake && (!archerIsAwake && !knightIsAwake));
+    }
+}
