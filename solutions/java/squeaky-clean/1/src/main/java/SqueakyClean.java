d14abf9267a32e42572130cf466cda316c832c57
Michael Durkan <durkanmick@gmail.com>
2025-09-07 08:49:01 +0000

    [Sync Iteration] java/squeaky-clean/1

---
+++ solutions/java/squeaky-clean/1/src/main/java/SqueakyClean.java
@@ -0,0 +1,48 @@
+class SqueakyClean {
+    static String clean(String identifier) {
+        identifier = replaceSpace(identifier);
+        identifier = replaceLeet(identifier);
+        StringBuilder sb = new StringBuilder(identifier);
+        sb = kebabToCamel(sb);
+        sb = removeSpecial(sb);
+        return sb.toString();
+    }
+
+    private static String replaceSpace(String identifier) {
+        return identifier.replace(' ', '_');      
+    }
+
+    private static String replaceLeet(String identifier) {
+        identifier = identifier.replace('4', 'a');
+        identifier = identifier.replace('3', 'e');
+        identifier = identifier.replace('0', 'o');
+        identifier = identifier.replace('1', 'l');
+        identifier = identifier.replace('7', 't');
+        
+        return identifier;
+    }
+    
+    private static StringBuilder kebabToCamel(StringBuilder sb) {
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '-') {
+                if (i != sb.length() - 1) {
+                    sb.setCharAt(i + 1, Character.toUpperCase(sb.charAt(i+1)));
+                    sb.deleteCharAt(i);
+                    i--;
+                }
+            }
+        }
+        return sb;
+    }
+
+    private static StringBuilder removeSpecial(StringBuilder sb) {
+        for (int i = 0; i < sb.length(); i++) {
+            if (!Character.isLetter(sb.charAt(i)) && sb.charAt(i) != '_') {
+                sb.deleteCharAt(i);   
+                i--;
+            }
+        }
+        return sb;
+    }
+    
+}
