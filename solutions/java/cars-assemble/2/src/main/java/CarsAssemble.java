6e3ede3d0bd4baf877962b1806534bfe3dfbb61a
Michael Durkan <durkanmick@gmail.com>
2025-09-07 05:43:05 +0000

    [Sync Iteration] java/cars-assemble/2

---
+++ solutions/java/cars-assemble/2/src/main/java/CarsAssemble.java
@@ -0,0 +1,29 @@
+public class CarsAssemble {
+
+    public static final int BASE_CARS_PER_HOUR = 221;
+    public static final int MINS_IN_HOUR = 60;
+
+    // Return success rate of input speed
+    public static double successRate(int speed) {
+        switch (speed) {
+            case 5: case 6: case 7: case 8:
+                return 0.9;
+            case 9:
+                return 0.8;
+            case 10:
+                return 0.77;
+            default:
+                return 1.0;
+        }
+    }
+    
+    // Calculate production rate from speed, base cars and success rate of given speed
+    public double productionRatePerHour(int speed) {
+        return speed * BASE_CARS_PER_HOUR * successRate(speed);
+    }
+
+    // Return cars per minute
+    public int workingItemsPerMinute(int speed) {
+        return (int) productionRatePerHour(speed) / MINS_IN_HOUR;
+    }
+}
