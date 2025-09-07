a06db03ab45589ed20bb4a1ceffc533090433f40
Michael Durkan <durkanmick@gmail.com>
2025-09-07 05:43:04 +0000

    [Sync Iteration] java/lasagna/4

---
+++ solutions/java/lasagna/4/src/main/java/Lasagna.java
@@ -0,0 +1,24 @@
+public class Lasagna {
+    public static final int EXPECTED_MINUTES = 40;
+    public static final int LASAGNE_LAYERS_PREP_TIME = 2;
+    
+    // Expected Minutes in Oven = 40
+    public int expectedMinutesInOven() {
+        return EXPECTED_MINUTES;
+    }
+    
+    // Remaining time in Oven = expected time - time so far
+    public int remainingMinutesInOven(int actualMinutes) {
+        return expectedMinutesInOven() - actualMinutes;
+    }
+    
+    // Preparation time = number of lasagne layers * 2
+    public int preparationTimeInMinutes(int numLayers) {
+        return numLayers * LASAGNE_LAYERS_PREP_TIME;
+    }
+    
+    // Total Cooking time = preparation time + minutes already in oven
+    public int totalTimeInMinutes(int numLayers, int minutesAlreadyInOven) {
+        return preparationTimeInMinutes(numLayers) + minutesAlreadyInOven;
+    }
+}
