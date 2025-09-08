736669675269cff75e6cbefc977b65ab26f063f2
Michael Durkan <durkanmick@gmail.com>
2025-09-08 05:56:35 +0000

    [Sync Iteration] java/need-for-speed/1

---
+++ solutions/java/need-for-speed/1/src/main/java/NeedForSpeed.java
@@ -0,0 +1,61 @@
+class NeedForSpeed {
+    private int speed;
+    private int batteryDrain;
+    private int distanceDriven;
+    private int battery;
+    
+    public NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        this.distanceDriven = 0;
+        this.battery = 100;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getBatteryDrain() {
+        return batteryDrain;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+    
+    public boolean batteryDrained() {
+        if (battery < batteryDrain) {
+            return true;
+        }
+        return false;
+    }
+
+    public int distanceDriven() {
+        return distanceDriven;
+    }
+
+    public void drive() {  
+        if (battery >= batteryDrain ) {
+            distanceDriven += speed;
+            battery -= batteryDrain;
+        }
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+    private int distance;
+    
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        int maxDrives = car.getBattery() / car.getBatteryDrain();
+        int maxDistance = maxDrives * car.getSpeed();
+        return maxDistance >= distance;
+    }
+}
