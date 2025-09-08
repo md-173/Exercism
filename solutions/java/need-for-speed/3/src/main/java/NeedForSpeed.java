61acd440ddd4ffbb26bc8f9774c23dccd8ce801d
Michael Durkan <durkanmick@gmail.com>
2025-09-08 08:20:48 +0000

    [Sync Iteration] java/need-for-speed/3

---
+++ solutions/java/need-for-speed/3/src/main/java/NeedForSpeed.java
@@ -0,0 +1,59 @@
+class NeedForSpeed {
+    private int speed;
+    private int batteryDrain;
+    private int distanceDriven;
+    private int battery;
+    
+    public NeedForSpeed(int speed, int batteryDrain) {
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        this.distanceDriven = 0;
+        this.battery = 100;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getBatteryDrain() {
+        return batteryDrain;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+    
+    public boolean batteryDrained() {
+        return battery < batteryDrain;
+    }
+
+    public int distanceDriven() {
+        return distanceDriven;
+    }
+
+    public void drive() {  
+        if (battery >= batteryDrain ) {
+            distanceDriven += speed;
+            battery -= batteryDrain;
+        }
+    }
+
+    public static NeedForSpeed nitro() {
+        return new NeedForSpeed(50, 4);
+    }
+}
+
+class RaceTrack {
+    private int distance;
+    
+    RaceTrack(int distance) {
+        this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+        while (!car.batteryDrained() && car.distanceDriven() < distance) {
+            car.drive();
+        }
+        return car.distanceDriven() >= distance;
+    }
+}
