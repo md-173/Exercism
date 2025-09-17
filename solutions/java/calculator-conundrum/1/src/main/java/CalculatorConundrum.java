1303c1019673c8ab3b047f1482491a22663d4f00
Michael Durkan <durkanmick@gmail.com>
2025-09-17 09:07:43 +0000

    [Sync Iteration] java/calculator-conundrum/1

---
+++ solutions/java/calculator-conundrum/1/src/main/java/CalculatorConundrum.java
@@ -0,0 +1,30 @@
+class CalculatorConundrum {
+    public String calculate(int operand1, int operand2, String operation) {
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "/":
+                try {
+                    result = operand1 / operand2;
+                } catch(ArithmeticException e) {
+                    throw new IllegalOperationException("Division by zero is not allowed", e);
+                }
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case null:
+                throw new IllegalArgumentException("Operation cannot be null");
+            case "":
+                throw new IllegalArgumentException("Operation cannot be empty");
+            default:
+                throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+        }
+        return String.format("%d %s %d = %d", operand1, operation, operand2, result);
+    }
+}
