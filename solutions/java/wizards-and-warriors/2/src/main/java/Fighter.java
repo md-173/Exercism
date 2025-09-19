cae63ff9884d3ada94731be1b9863a01dd391312
Michael Durkan <durkanmick@gmail.com>
2025-09-19 10:08:17 +0000

    [Sync Iteration] java/wizards-and-warriors/2

---
+++ solutions/java/wizards-and-warriors/2/src/main/java/Fighter.java
@@ -0,0 +1,54 @@
+class Fighter {
+
+    boolean isVulnerable() {
+        return true;
+    }
+
+    int getDamagePoints(Fighter fighter) {
+        return 1;
+    }
+}
+
+class Warrior extends Fighter {  
+
+    @Override
+    boolean isVulnerable() {
+        return false;
+    }
+
+    @Override
+    int getDamagePoints(Fighter fighter) {
+        int dmg = fighter.isVulnerable() ? 10 : 6;
+        return dmg;
+    }
+
+    @Override
+    public String toString() {
+        return "Fighter is a Warrior";
+    }
+}
+
+class Wizard extends Fighter {
+    private boolean prepared = false;
+
+    @Override
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+    void prepareSpell() {
+        prepared = true;
+    }
+
+    @Override
+    boolean isVulnerable() {
+        boolean vuln = prepared ? false : true;
+        return vuln;
+    }
+
+    @Override
+    int getDamagePoints(Fighter fighter) {
+        int dmg = prepared ? 12 : 3;
+        return dmg;
+    }
+}
