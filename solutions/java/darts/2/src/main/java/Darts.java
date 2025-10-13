590fcc8d5ae1f128bc58a3dbab14c60d5d9ff64f
Michael Durkan <durkanmick@gmail.com>
2025-10-13 10:40:22 +0000

    [Sync Iteration] java/darts/2

---
+++ solutions/java/darts/2/src/main/java/Darts.java
@@ -0,0 +1,6 @@
+class Darts {
+    int score(double xOfDart, double yOfDart) {
+        double d = Math.sqrt(Math.pow(xOfDart,2) + Math.pow(yOfDart,2));
+        return (d > 10) ? 0 : (d <= 10 && d > 5) ? 1 : (d <= 5 && d > 1) ? 5 : 10;
+    }
+}
